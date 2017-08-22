c57f7543c27966637eeef9ad11eb94dafb283c7f
alaninmcr <alan.r.williams@manchester.ac.uk>
2017-08-22 22:21:34 +0000

    Switched serializer

--- app/serializers/pcs_serializer.rb
+++ app/serializers/pcs_serializer.rb
@@ -1,10 +1,12 @@
 class PCSSerializer < BaseSerializer
   has_many :creators, include_data:true
-  has_one :submitter, include_data:true do
-    determine_submitter object
-  end
+  has_one :submitter
   has_one :policy, include_data:true
   attribute :tags do
     serialize_annotations(object)
+  end
+
+  def submitter
+    determine_submitter object
   end
 end
