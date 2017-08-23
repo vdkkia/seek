cae2e2d7ce58ff02f32bec72e338ad891548cc15
Hadas Leonov <blackbirdy@gmail.com>
2017-08-23 14:10:07 +0200

    re-serialize error codes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -192,11 +192,7 @@ class ApplicationController < ActionController::Base
             redirect_to path
           end
         end
-        format.json {
-          errors = [{"title": "Unauthorized", "status": "401", "detail": flash[:error].to_s}]
-          render json: errors, status: 401
-         # render json: { status: 401, error_message: flash[:error] }
-        }
+        format.json { render json: {"title": "Unauthorized", "detail": flash[:error].to_s}, status: :unauthorized, status: :unauthorized}
       end
     end
   end
@@ -279,10 +275,7 @@ class ApplicationController < ActionController::Base
         flash[:error] = "The #{name.humanize} does not exist!"
         format.rdf { render text: 'Not found', status: :not_found }
         format.xml { render text: '<error>404 Not found</error>', status: :not_found }
-        format.json {
-          errors = [{"title": "Not found", "status": "404"}]
-          render json: errors, status: :not_found
-        }
+        format.json { render json: {"title": "Not found", status: :not_found}, status: :not_found }
         format.html { redirect_to eval "#{controller_name}_path" }
       end
     else
@@ -319,10 +312,9 @@ class ApplicationController < ActionController::Base
         end
         format.rdf { render text: "You may not #{action} #{name}:#{params[:id]}", status: :forbidden }
         format.xml { render text: "You may not #{action} #{name}:#{params[:id]}", status: :forbidden }
-        format.json {
-          errors = [{"title": "Forbidden", "status": "403", "detail": "You may not #{action} #{name}:#{params[:id]}"}]
-          render json: errors, status: :forbidden
-        }
+        format.json { render json: {"title": "Forbidden",
+                                    "detail": "You may not #{action} #{name}:#{params[:id]}",
+                                     status: :forbidden}, status: :forbidden }
       end
       return false
     end
@@ -345,10 +337,7 @@ class ApplicationController < ActionController::Base
 
       format.rdf { render text: 'Not found', status: :not_found }
       format.xml { render text: '<error>404 Not found</error>', status: :not_found }
-      format.json {
-        errors = [{"title": "Not found", "status": "404"}]
-        render json: errors, status: :not_found
-      }
+      format.json { render json: {"title": "Not found", status: :not_found}, status: :not_found }
     end
     false
   end
@@ -583,6 +572,7 @@ class ApplicationController < ActionController::Base
 
   def check_illegal_id
     begin
+      Rails.logger.info("checking illegal ID")
       if !params[:id].nil?
         raise ArgumentError.new('A POST request is not allowed to specify an id')
       end
